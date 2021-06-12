bed7619328795a5cdb6e5f6c885f03ddf1466479
Fer96carvalho <f.luiz1996@gmail.com>
2021-06-11 22:52:49 -0400

    Exercicio PG Arrray 8/06

---
+++ array.js
@@ -0,0 +1,45 @@
+let DjsEDM = ["AVICII", "DAVIDGUETTA", "ALOK", "LIU", "MANDRAGORA"];
+
+DjsEDM.push("TRITONAL","HIPENESS");
+// Usando o push adicionamos, novos itens ao array DjsEDM, eles são adicionados ao final do array
+console.log(DjsEDM)
+console.log("O push adiciona um novo item no array")
+console.log(DjsEDM.join())
+
+DjsEDM.pop;
+//pop retira o ultimo iten do array e pode retornar ele se voce usar o console.log
+console.log("O pop apaga o ultimo iten do array")
+console.log(DjsEDM.join())
+
+DjsEDM.shift;
+//shift retira o primeiro item de array e pode retornar ele se voce usar o conlose.log
+console.log("O shift retira o primeiro iten do array")
+console.log(DjsEDM.join())
+
+DjsEDM.unshift("AVICII");
+// Usando o unshift, adicionamos itens ao ainicio do array
+console.log("O unshift adiciona um iten no inicio dos itens do array")
+console.log(DjsEDM.join())
+
+DjsEDM.indexOf("DAVIDGUETTA")
+// Procura uma informação no array e retorna a sua posição
+console.log("O indexof procura a posição do iten no array")
+console.log(DjsEDM.join())
+
+DjsEDM.lastIndexOf("AVICII")
+// Procura uma informação mas de trás para frente e retorna a posição do iten no array
+console.log(DjsEDM.lastIndexOf("AVICII"))
+
+
+DjsEDM.join
+// lista todos os itens do array em string(texto) e você pode escolher o separador.
+console.log(DjsEDM.join())
+
+DjsEDM.length
+// Lista o total de itens no array
+console.log(DjsEDM.length)
+
+DjsEDM.includes()
+// Faz uma busca para saber se o elemento está presente no array
+console.log(DjsEDM.includes("AVICII"))
+
