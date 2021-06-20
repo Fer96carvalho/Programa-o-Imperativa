65afcab7c9514ab51ce1205d35e9a9da9833a09e
Fer96carvalho <f.luiz1996@gmail.com>
2021-06-20 13:40:05 -0400

    Exercicio PG Funções avançadas

---
+++ MicroDsesafioArrow.js
@@ -0,0 +1,19 @@
+// function print(mensagem){
+//     console.log(mensagem);
+// }
+// print("Ola, bom dia");
+
+// function soma(n1,n2){
+//     return n1 + n2
+// }
+
+// console.log(soma(10,20));
+
+let print = mensagem => console.log(mensagem);
+
+print("Olá, bom dia!!");
+
+let soma =(n1,n2) => n1+n2;
+
+console.log(soma(10,20));
+
